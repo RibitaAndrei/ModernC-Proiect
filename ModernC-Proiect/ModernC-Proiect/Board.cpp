3928ad7aa5bd63d928d97390b12567d9d943940e
RibitaAndrei <147505124+RibitaAndrei@users.noreply.github.com>
2023-10-29 19:59:59 +0200

    Am implementat o functie de initializare a tablei de joc.

--- ModernC-Proiect/ModernC-Proiect/Board.cpp
+++ ModernC-Proiect/ModernC-Proiect/Board.cpp
@@ -1,4 +1,4 @@
-#include "board.h"
+﻿#include "board.h"
 #include <iostream>
 
 Board::Board(int size) 
@@ -28,5 +28,25 @@ void Board::setCell(int row, int col, const Foundation& value)
     if (row >= 0 && row < m_size && col >= 0 && col < m_size) 
     {
         m_board[row][col] = value;
+    }
+}
+
+void Board::Initialize()
+{
+    m_board.clear();
+
+    // Inițializează m_board cu un vector de m_size x m_size Foundation-uri.
+    m_board.resize(m_size, std::vector<Foundation>(m_size, Foundation()));
+
+    // Umple tabla de joc 
+    for (int i = 0; i < m_size; ++i) {
+        for (int j = 0; j < m_size; ++j) {
+            if (i % 2 == 0 && j % 2 == 0) {
+                m_board[i][j].setVertical(true);
+            }
+            else if (i % 2 == 1 && j % 2 == 1) {
+                m_board[i][j].setHorizontal(true);
+            }
+        }
     }
 }
