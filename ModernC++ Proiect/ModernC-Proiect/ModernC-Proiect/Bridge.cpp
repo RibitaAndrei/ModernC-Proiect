cfce8e15fae738729931b8b371d092886a8052e9
Alex Popa <alexpopa1910@gmil.com>
2023-10-29 23:28:47 +0200

    Am implementat get-erii si set-erii

--- ModernC++ Proiect/ModernC-Proiect/ModernC-Proiect/Bridge.cpp
+++ ModernC++ Proiect/ModernC-Proiect/ModernC-Proiect/Bridge.cpp
@@ -1,1 +1,21 @@
 #include "Bridge.h"
+
+Pilon Bridge::getPylon1() const
+{
+    return pilons.first;
+}
+
+Pilon Bridge::getPylon2() const
+{
+    return pilons.second;
+}
+
+void Bridge::setPylon1(Pilon pilon1)
+{
+    pilons.first = pilon1;
+}
+
+void Bridge::setPylon2(Pilon pilon2)
+{
+    pilons.second = pilon2;
+}
