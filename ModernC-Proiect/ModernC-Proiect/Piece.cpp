39def0395021c6d72223d095758f14a9841eb60d
Alex Popa <alexpopa1910@gmil.com>
2023-11-05 22:16:23 +0200

    Am mutat implementarile functiilor claselor Bridge si Pilon in modul

---
+++ ModernC-Proiect/ModernC-Proiect/Piece.cpp
@@ -0,0 +1,62 @@
+module piece;
+
+char pieces::Pilon::getColor() const
+{
+	return m_color;
+}
+
+void pieces::Pilon::setColor(char color)
+{
+	m_color = color;
+}
+
+int pieces::Pilon::getPositionX() const
+{
+	return m_position.first;
+}
+
+void pieces::Pilon::setPositionX(int posX)
+{
+	m_position.first = posX;
+}
+
+int pieces::Pilon::getPositionY() const
+{
+	return m_position.second;
+}
+
+void pieces::Pilon::setPositionY(int posY)
+{
+	m_position.second = posY;
+}
+
+std::pair<int, int> pieces::Pilon::getPositionXY() const
+{
+	return m_position;
+}
+
+void pieces::Pilon::setPositionXY(int posX, int posY)
+{
+	m_position.first = posX;
+	m_position.second = posY;
+}
+
+pieces::Pilon pieces::Bridge::getPylon1() const
+{
+	return pilons.first;
+}
+
+pieces::Pilon pieces::Bridge::getPylon2() const
+{
+	return pilons.second;
+}
+
+void pieces::Bridge::setPylon1(Pilon pilon1)
+{
+	pilons.first = pilon1;
+}
+
+void pieces::Bridge::setPylon2(Pilon pilon2)
+{
+	pilons.second = pilon2;
+}
