bbfde9849967855fa10aafefd3d69686e08bf3e3
Popi03 <148215192+Popi03@users.noreply.github.com>
2023-11-05 13:10:00 +0200

    Am creat constructorii si destructorul pentru clasa Game

--- ModernC-Proiect/ModernC-Proiect/Game.cpp
+++ ModernC-Proiect/ModernC-Proiect/Game.cpp
@@ -1,1 +1,15 @@
 #include "Game.h"
+// Constructor cu parametri
+Game::Game(int boardSize, std::string playerName1, std::string playerName2) :
+    m_gameBoard(boardSize), m_player1(playerName1, 'r'), m_player2(playerName2, 'b')
+{
+}
+// Constructor de copiere
+Game::Game(const Game& copy) :
+    m_gameBoard(copy.m_gameBoard), m_player1(copy.m_player1), m_player2(copy.m_player2)
+{
+}
+// Destructor
+Game::~Game()
+{
+}
