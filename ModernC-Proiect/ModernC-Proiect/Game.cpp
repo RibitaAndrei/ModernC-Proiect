553c9b1db7e09dd23efacb90baea96e331b78cd9
Popi03 <148215192+Popi03@users.noreply.github.com>
2023-11-05 13:12:23 +0200

    Am implementat get-erii si set-erii clasei Game

--- ModernC-Proiect/ModernC-Proiect/Game.cpp
+++ ModernC-Proiect/ModernC-Proiect/Game.cpp
@@ -1,4 +1,4 @@
-#include "Game.h"
+﻿#include "Game.h"
 // Constructor cu parametri
 Game::Game(int boardSize, std::string playerName1, std::string playerName2) :
     m_gameBoard(boardSize), m_player1(playerName1, 'r'), m_player2(playerName2, 'b')
@@ -12,4 +12,41 @@ Game::Game(const Game& copy) :
 // Destructor
 Game::~Game()
 {
+}
+
+// Setează dimensiunea tablei de joc
+void Game::SetBoardSize(int size)
+{
+    m_gameBoard.Initialize(); // Inițializează tabla cu dimensiunea specificată
+    m_gameBoard.Reset(); // Resetează tabla
+}
+
+// Setează numele primului jucător
+void Game::SetFirstPlayerName(std::string name)
+{
+    m_player1.SetPlayerName(name);
+}
+
+// Setează numele celui de-al doilea jucător
+void Game::SetSecondPlayerName(std::string name)
+{
+    m_player2.SetPlayerName(name);
+}
+
+// Obține dimensiunea tablei de joc
+int Game::GetBoardSize()
+{
+    return m_gameBoard.GetBoard().size();
+}
+
+// Obține numele primului jucător
+std::string Game::GetFirstPlayerName()
+{
+    return m_player1.GetPlayerName();
+}
+
+// Obține numele celui de-al doilea jucător
+std::string Game::GetSecondPlayerName()
+{
+    return m_player2.GetPlayerName();
 }
