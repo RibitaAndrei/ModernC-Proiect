a0455847f0c7f2e10e9079253be26d2d480d093c
mattgorbava <matei.ionescu@student.unitbv.ro>
2023-11-26 16:31:49 +0200

    Clasa Pilon mosteneste clasa Foundation

--- ModernC-Proiect/ModernC-Proiect/Pilon.h
+++ ModernC-Proiect/ModernC-Proiect/Pilon.h
@@ -1,27 +1,16 @@
 #pragma once
 #include <utility>
+#include "Foundation.h"
 
-class Pilon
+class Pilon : public Foundation
 {
-private:
-	char m_color;
-	std::pair <int, int> m_position;
-
 public:
-	Pilon(char color);
+	Pilon();
+	Pilon(Foundation::PlayerColor color, Foundation::Position position);
 	Pilon& operator=(const Pilon& copy) = default;
 	Pilon(const Pilon& copy) = default;
 	~Pilon() = default;
 
-	char GetColor() const;
-	void SetColor(char color);
-
-	int GetPositionX() const;
-	void SetPositionX(int posX);
-
-	int GetPositionY() const;
-	void SetPositionY(int posY);
-
-	std::pair<int, int> GetPositionXY() const;
-	void SetPositionXY(int posX, int posY);
+	virtual Foundation::PlayerColor GetColor() const override;
+	virtual void SetColor(const Foundation::PlayerColor& color) override;
 };
