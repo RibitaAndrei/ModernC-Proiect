3cf89b9d27a749ff05afb4e21c8b53430bff6053
mattgorbava <matei.ionescu@student.unitbv.ro>
2023-11-26 17:05:42 +0200

    Modificari la get-eri in Game

--- ModernC-Proiect/ModernC-Proiect/Game.h
+++ ModernC-Proiect/ModernC-Proiect/Game.h
@@ -11,23 +11,33 @@ public:
 	Game(const Game& copy);
 	~Game();
 
-	void SetBoardSize(int size);
-	void SetFirstPlayerName(std::string name);
-	void SetSecondPlayerName(std::string name);
-	
-	int GetBoardSize();
-	std::string GetFirstPlayerName();
-	std::string GetSecondPlayerName();
+	enum class ActivePlayer//nou
+	{
+		Player1,
+		Player2
+	};
 
-	void PauseMenu();
+	void SetBoardSize(int size);
+	void SetFirstPlayerName(const std::string& name);
+	void SetSecondPlayerName(const std::string& name);
+
+	int GetBoardSize() const;
+	std::string GetFirstPlayerName() const;
+	std::string GetSecondPlayerName() const;
+
+	int GetScorePlayer1() const;
+	int GetScorePlayer2() const;
+
+	void SetScorePlayer1(const int& score);
+	void SetScorePlayer2(const int& score);
+
+	void AddScorePlayer1();
+	void AddScorePlayer2();
 
 	bool PlacePiece(const std::pair<int, int>& coordinates);
-	
-	void DisplayRules() const;
 
 	void StartGame();
-	void StartRound();
-	bool CheckWinCondition() const;
+	//bool CheckWinCondition() const;
 
 	void ResetGame();
 
@@ -35,8 +45,10 @@ private:
 	Player m_player1, m_player2;
 	Board m_gameBoard;
 
-	bool m_gameFinished;
+	int m_scorePlayer1;
+	int m_scorePlayer2;
 
-	void DisplayScore();
+	bool m_gameFinished;
+	ActivePlayer m_activePlayer;//nou
 };
 
