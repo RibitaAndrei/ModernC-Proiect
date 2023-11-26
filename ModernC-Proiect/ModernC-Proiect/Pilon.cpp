545ed24e915a64d38f1804ee9de7739b06e0dc7e
mattgorbava <matei.ionescu@student.unitbv.ro>
2023-11-26 16:34:02 +0200

    Am implementat constructorii pentru clasa Pilon

--- ModernC-Proiect/ModernC-Proiect/Pilon.cpp
+++ ModernC-Proiect/ModernC-Proiect/Pilon.cpp
@@ -1,47 +1,12 @@
 #include "Pilon.h"
 
-Pilon::Pilon(char color)
-    :m_color{color}
+Pilon::Pilon()
+    :Foundation()
 {
 }
 
-char Pilon::GetColor() const
+Pilon::Pilon(Foundation::PlayerColor color, Foundation::Position position)
+    :Foundation(position, color)
 {
-    return m_color;
 }
 
-void Pilon::SetColor(char color)
-{
-    m_color = color;
-}
-
-int Pilon::GetPositionX() const
-{
-    return m_position.first;
-}
-
-void Pilon::SetPositionX(int posX)
-{
-    m_position.first = posX;
-}
-
-int Pilon::GetPositionY() const
-{
-    return m_position.second;
-}
-
-void Pilon::SetPositionY(int posY)
-{
-    m_position.second = posY;
-}
-
-std::pair<int, int> Pilon::GetPositionXY() const
-{
-    return m_position;
-}
-
-void Pilon::SetPositionXY(int posX, int posY)
-{
-    m_position.first = posX;
-    m_position.second = posY;
-}
