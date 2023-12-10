87b2c2e4598ed9f835c2c4e75a25894d512ba774
mattgorbava <imatei2003@gmail.com>
2023-12-10 12:00:44 +0200

    Position se muta in pilon, fiind importanta doar acolo

--- ModernC-Proiect/ModernC-Proiect/Foundation.h
+++ ModernC-Proiect/ModernC-Proiect/Foundation.h
@@ -3,7 +3,6 @@
 class Foundation
 {
 public:
-	using Position = std::pair<size_t, size_t>;
 	enum class PlayerColor
 	{
 		NoColor,
@@ -12,25 +11,14 @@ public:
 	};
 
 	Foundation();
-	Foundation(Position position);
-	Foundation(Position position, PlayerColor color);
 	Foundation& operator=(const Foundation& copy) = default;
 	Foundation(const Foundation& copy) = default;
 	virtual ~Foundation() = default;
 	
-	virtual Position GetPosition() const;
-	virtual void SetPosition(Position& position);
-
-	virtual size_t GetRow() const;
-	virtual void SetRow(size_t& row);
-
-	virtual size_t GetColumn() const;
-	virtual void SetColumn(size_t& col);
-
 	virtual PlayerColor GetColor() const = 0;
 	virtual void SetColor(const PlayerColor& color) = 0;
+
 protected:
-	Position m_position;
 	PlayerColor m_color;
 };
 
--- ModernC-Proiect/ModernC-Proiect/Pilon.h
+++ ModernC-Proiect/ModernC-Proiect/Pilon.h
@@ -5,12 +5,26 @@
 class Pilon : public Foundation
 {
 public:
+	using Position = std::pair<size_t, size_t>;
+
 	Pilon();
-	Pilon(Foundation::PlayerColor color, Foundation::Position position);
+	Pilon(Foundation::PlayerColor color, Position position);
 	Pilon& operator=(const Pilon& copy) = default;
 	Pilon(const Pilon& copy) = default;
 	~Pilon() = default;
 
+	virtual Position GetPosition() const;
+	virtual void SetPosition(Position& position);
+
+	virtual size_t GetRow() const;
+	virtual void SetRow(size_t& row);
+
+	virtual size_t GetColumn() const;
+	virtual void SetColumn(size_t& col);
+
 	virtual Foundation::PlayerColor GetColor() const override;
 	virtual void SetColor(const Foundation::PlayerColor& color) override;
+
+private:
+	Position m_position;
 };
