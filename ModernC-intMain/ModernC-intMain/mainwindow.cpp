18005d2b2c3214a61ba9da070fa0732cf30ea183
Popi03 <148215192+Popi03@users.noreply.github.com>
2023-12-17 13:52:53 +0200

    Am implementat constructorul mainwindow

--- ModernC-intMain/ModernC-intMain/mainwindow.cpp
+++ ModernC-intMain/ModernC-intMain/mainwindow.cpp
@@ -1,7 +1,15 @@
 #include "mainwindow.h"
 
 mainwindow::mainwindow(QWidget* parent, const int& boardSize)
+    : QMainWindow(parent),
+    m_windowHeight{ 2 * kVerticalMarginSize + boardSize * kSquareSize },
+    m_windowWidth{ 2 * kHorizontalMarginSize + boardSize * kSquareSize },
+    m_boardSizeRects{ boardSize },
+    m_boardSizePixels{ m_boardSizeRects * kSquareSize }
 {
+    m_game.SetBoardSize(boardSize);
+    this->setFixedSize(m_windowWidth, m_windowHeight);
+    ui.setupUi(this);
 
 }
 
