a6a9b98857e6738df41d52166c2ac59c714f4640
Alex Popa <alexpopa1910@gmil.com>
2024-01-03 18:51:47 +0200

    Modificari clasa Player

--- ModernC-intMain/ModernC-intMain/Player.cpp
+++ ModernC-intMain/ModernC-intMain/Player.cpp
@@ -1,10 +1,13 @@
 #include "Player.h"
 
-Player::Player(char teamColor)
+Player::Player(IPiece::PlayerColor teamColor)
+	:m_teamColor{ teamColor },
+	m_pilonCounter{ 50 },
+	m_bridgeCounter{ 50 }
 {
 }
 
-Player::Player(std::string playerName, Foundation::PlayerColor teamColor)
+Player::Player(std::string playerName, IPiece::PlayerColor teamColor)
 	:m_pilonCounter{ 50 },
 	m_bridgeCounter{ 50 },
 	m_teamColor{ teamColor },
@@ -12,7 +15,7 @@ Player::Player(std::string playerName, Foundation::PlayerColor teamColor)
 {
 }
 
-Foundation::PlayerColor Player::GetColor() const
+IPiece::PlayerColor Player::GetColor() const
 {
 
 	return m_teamColor;
@@ -33,7 +36,7 @@ std::string Player::GetPlayerName() const
 	return m_playerName;
 }
 
-void Player::SetColor(const Foundation::PlayerColor& color)
+void Player::SetColor(const IPiece::PlayerColor& color)
 {
 	m_teamColor = color;
 }
@@ -63,12 +66,30 @@ void Player::SetPilonCounter(const int& count)
 	m_pilonCounter = count;
 }
 
+void Player::AddPilon(IPiece* pilon)
+{
+	m_pilons.push_back(pilon);
+	DecrementPilons();
+}
+
+void Player::AddBridge(IPiece* firstPilon, IPiece* secondPilon, Player* activePlayer)
+{
+	m_bridges.push_back(new Bridge(firstPilon, secondPilon, activePlayer->GetColor()));
+	DecrementBridges();
+}
+
+IPiece* Player::GetLastBridge() const
+{
+	if (!m_bridges.empty())
+		return m_bridges[m_bridges.size() - 1];
+}
+
 void Player::SetNextPlayer(Player& player)
 {
 	m_nextPlayer = &player;
 }
 
-Player* Player::GetNextPlayer()
+Player* Player::GetNextPlayer() const
 {
 	return m_nextPlayer;
 }
--- ModernC-intMain/ModernC-intMain/Player.h
+++ ModernC-intMain/ModernC-intMain/Player.h
@@ -1,40 +1,46 @@
 #pragma once
 #include <string>
 #include<vector>
-#include "Foundation.h"
+#include "IPiece.h"
 #include "Pilon.h"
 #include "Bridge.h"
 //import piece;
 class Player
 {
 private:
-	Foundation::PlayerColor m_teamColor;
-	int m_pilonCounter, m_bridgeCounter;
+	IPiece::PlayerColor m_teamColor;
+	uint8_t m_pilonCounter;
+	uint8_t m_bridgeCounter;
 	std::string m_playerName;
-	std::vector <Pilon> m_pilons;
-	std::vector <Bridge> m_bridges;
+	std::vector <IPiece*> m_pilons;
+	std::vector <IPiece*> m_bridges;
 	Player* m_nextPlayer;
 
 public:
-	Player(char teamColor);
-	Player(std::string playerName, Foundation::PlayerColor teamColor);
+	Player(IPiece::PlayerColor teamColor);
+	Player(std::string playerName, IPiece::PlayerColor teamColor);
 	Player& operator=(const Player& copy) = default;
 	Player(const Player& copy) = default;
 	~Player() = default;
 
-	Foundation::PlayerColor GetColor() const;
+	IPiece::PlayerColor GetColor() const;
 	int GetPilonCounter() const;
 	int GetBridgeCounter() const;
 	std::string GetPlayerName() const;
 
-	void SetColor(const Foundation::PlayerColor& color);
-	void DecrementPilons();
-	void DecrementBridges();
+	void SetColor(const IPiece::PlayerColor& color);
 	void SetPlayerName(const std::string& name);
 	void SetBridgeCounter(const int& count);
 	void SetPilonCounter(const int& count);
 
-	void SetNextPlayer(Player& player);
-	Player* GetNextPlayer();
-};
+	void DecrementPilons();
+	void DecrementBridges();
 
+	void AddPilon(IPiece* pilon);
+	void AddBridge(IPiece* firstPilon, IPiece* secondPilon, Player* activePlayer);
+
+	IPiece* GetLastBridge() const;
+
+	void SetNextPlayer(Player& player);
+	Player* GetNextPlayer() const;
+};
